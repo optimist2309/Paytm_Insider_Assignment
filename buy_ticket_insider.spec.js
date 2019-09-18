eade852022a5c36973acac24e810e185415df4db
HARISH KUMAR  TIWARI <harishtiwary46@gmail.com>
2019-09-18 09:37:49 +0530

    Update buy_ticket_insider.spec.js with comments .

--- buy_ticket_insider.spec.js
+++ buy_ticket_insider.spec.js
@@ -3,9 +3,17 @@
 it('Automated test cases to buy the tickets from insider.com in featured events from selected city', () => {
     
     cy.visit("https://insider.in/")
+
+    // Selecting the pune city from city dropdown.
     cy.get('.city-list > :nth-child(7) > a').click()
+    
+    // Selecting the bacardi weekender from featured event section.
     cy.get('.carousel-item-1 > .featured-card > a > .featured-card-details > .featured-card-details-top > .featured-card-name').click({ force: true })
+    
+    // Closing the subscribe news letter popup
     cy.get('#closeModalButton').click()
+    
+    // Clicking on buy and selecting the 1 ticket to buy 
     cy.get('.header-detail > .action-button-wrapper > .action-button-btns-row > .action-button-main').click()
     cy.get(':nth-child(1) > .show-partial > .show-partial-content > .buyflow-action > .buyflow-buy > .marionette-spa-btn > .base-button').click()
     cy.get(':nth-child(1) > .show-partial > .show-partial-content > .buyflow-action > .buyflow-price > .main-item-select-wrapper > .main-item-select').select('1')
